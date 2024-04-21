9dec776c49fd6b10e1b35d42ef14cf6035f8083e
abdulmuhd <abdulmuhd0@gmail.com>
2024-04-21 15:08:52 +0100

    update

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from 'express'
 import ask from './src/api/ask.js'
 import { config } from 'dotenv'
+import cors from 'cors';
 
 config()
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use('/api/ask', ask)
 app.use('/', (req, res) => {
-  res.send('working..')
+  res.send('Welcome to Danmasani AI')
 })
 
 
