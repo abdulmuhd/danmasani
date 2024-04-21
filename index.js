8793ed3a423a34668a89236e2fdebb92c7f9fe60
abdulmuhd <abdulmuhd0@gmail.com>
2024-04-21 15:34:21 +0100

    update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use('/api/ask', ask)
 app.use('/', (req, res) => {
   res.send('Welcome to Danmasani AI')
