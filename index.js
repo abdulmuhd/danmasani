3537c36dfc40149a161e019717fa8683d493cbfa
abdulmuhd <abdulmuhd0@gmail.com>
2024-04-22 12:09:25 +0100

    update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import ask from './src/api/ask.js'
-import eng from './src/api/eng.js';
+//import eng from './src/api/eng.js';
 import { config } from 'dotenv'
 import cors from 'cors';
 
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/ask', ask)
-app.use('/api/eng', eng)
+//app.use('/api/eng', eng)
 app.use('/', (req, res) => {
   res.send('Welcome to Danmasani AI')
 })
